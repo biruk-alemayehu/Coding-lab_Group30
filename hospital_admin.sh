2d0387971374d0763f2a09b3d4a463e0e09ad9f9
UkweziKutazima <k.ndayiragi@alustudent.com>
2026-09-25 14:02:32 +0300

    we added directory checks and creation using for loop logic

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -2,4 +2,11 @@
 
 initialize_system() {
     directories=("active_logs" "archived_logs" "reports")
+
+    for dir in "#{directories[@]}"; do
+        if [ ! -d "$dir"  ]; then
+             echo "Creating the directory.."
+             mkdir "$dir"
+        fi
+    done
 }
