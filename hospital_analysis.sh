dd56604bd67a230e03c75e956270bdd4a6182d47
Jazzy Bruno <jazzybruno45@gmail.com>
2026-09-26 12:07:12 +0200

    updates: added process_vitals to extract critical rows

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -5,3 +5,8 @@ REPORT_DIR="reports"
 HEART_LOG="$LOG_DIR/heart_rate_log.log"
 TEMP_LOG="$LOG_DIR/temperature_log.log"
 WATER_LOG="$LOG_DIR/water_usage_log.log"
+
+process_vitals() {
+    mkdir -p "$REPORT_DIR"
+    grep -h "CRITICAL" "$HEART_LOG" "$TEMP_LOG" | awk -F' \\| ' '{print $1, "|", $2, "|", $3}' > "$REPORT_DIR/critical_alerts.txt"
+}
