886cdeccbc61d9ddfa1e84af21749ade2b7822fe
Jazzy Bruno <jazzybruno45@gmail.com>
2026-09-26 12:07:06 +0200

    updates: created hospital_analysis.sh with log paths

---
+++ hospital_analysis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+LOG_DIR="active_logs"
+REPORT_DIR="reports"
+HEART_LOG="$LOG_DIR/heart_rate_log.log"
+TEMP_LOG="$LOG_DIR/temperature_log.log"
+WATER_LOG="$LOG_DIR/water_usage_log.log"
